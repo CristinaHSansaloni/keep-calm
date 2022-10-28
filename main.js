001312696f081a6fc3d51975cc9e160a38b43afc
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-10-28 12:27:20 +0200

    Open/Close menu

--- main.js
+++ main.js
@@ -2,74 +2,130 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.ham-menu');
 const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector('.inactive');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
-menuEmail.addEventListener('click', () => {
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleShopMenu);
+productDetailCloseIcon.addEventListener('click', closeMenuIcon);
+
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-    aside.classList.add('inactive');
-});    
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+}
 
-menuHamIcon.addEventListener('click', () => {
-    aside.classList.add('inactive');
+function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
-});
+    desktopMenu.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+}
 
-menuShoppingCart.addEventListener('click', () => {
+function toggleShopMenu() {
+    shoppingCartContainer.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
-    aside.classList.toggle('inactive');
-    
-});
+    productDetailContainer.classList.add('inactive');
+}
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+function closeMenuIcon() {
+    shoppingCartContainer.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+}
 
 const productList = [];
 productList.push ({
-    name:'Bike',
-    price: 12700,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    name:'Volcanic stones',
+    price: 69,
+    image: 'https://images.pexels.com/photos/6560276/pexels-photo-6560276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
 productList.push ({
-    name:'Bicycle helmet',
-    price: 1200,
-    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+    name:'Plant',
+    price: 60,
+    image: 'https://images.pexels.com/photos/1542937/pexels-photo-1542937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
 productList.push ({
-    name:'Bicycle helmet',
-    price: 1600,
-    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+    name:'Pilates Ball',
+    price: 49,
+    image: 'https://images.pexels.com/photos/3757374/pexels-photo-3757374.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
 productList.push ({
-    name:'Bicycle helmet',
-    price: 1500,
-    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+    name:'Budha',
+    price: 100,
+    image: 'https://images.pexels.com/photos/4203074/pexels-photo-4203074.jpeg'
 });
 productList.push ({
-    name:'Seat',
+    name:'Dried flowers',
+    price: 20,
+    image: 'https://images.pexels.com/photos/3865732/pexels-photo-3865732.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Soy Candle',
+    price: 15,
+    image: 'https://images.pexels.com/photos/6311861/pexels-photo-6311861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Dry Body Brushing Set',
+    price: 139,
+    image: 'https://images.pexels.com/photos/6621281/pexels-photo-6621281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Tibetan Singing Bowl',
+    price: 200,
+    image: 'https://images.pexels.com/photos/8391651/pexels-photo-8391651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Esential Oil Diffuser',
+    price: 120,
+    image: 'https://images.pexels.com/photos/5380001/pexels-photo-5380001.jpeg'
+});
+productList.push ({
+    name:'Buddhist Japa Mala Beads',
+    price: 40,
+    image: 'https://images.pexels.com/photos/10605935/pexels-photo-10605935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Yoga mat',
+    price: 15,
+    image: 'https://images.pexels.com/photos/6740756/pexels-photo-6740756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Roller & Gua Sha',
+    price: 35,
+    image: 'https://images.pexels.com/photos/6621435/pexels-photo-6621435.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Matcha Tea Kit',
     price: 300,
-    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+    image: 'https://images.pexels.com/photos/8951765/pexels-photo-8951765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
 productList.push ({
-    name:'Tennis Montain Bike',
-    price: 2200,
-    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
-});
-productList.push ({
-    name:'Sunglasses',
-    price: 800,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
-});
-productList.push ({
-    name:'Sunglasses',
-    price: 600,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
-});
-productList.push ({
-    name:'Bicycle seat bag',
-    price: 876,
-    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+    name:'Japanese Painting Kit',
+    price: 45,
+    image: 'https://images.pexels.com/photos/8844004/pexels-photo-8844004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 }); 
+productList.push ({
+    name:'Projector with white noises',
+    price: 159,
+    image: 'https://images.pexels.com/photos/2106768/pexels-photo-2106768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push ({
+    name:'Wooden Puzzle 3D',
+    price: 49,
+    image: 'https://images.pexels.com/photos/7296663/pexels-photo-7296663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
 
 
 productList.map(function (product){ 
@@ -78,6 +134,7 @@ productList.map(function (product){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -107,45 +164,6 @@ productList.map(function (product){
         cardsContainer.appendChild(productCard);
   });
 
-/*function renderProducts(arr) {
-    for (product of arr) {
-        const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
-    
-        const productImg = document.createElement('img');
-        productImg.setAttribute('src', product.image);
-    
-        const productInfo = document.createElement('div');
-        productInfo.classList.add('product-info');
-    
-        const productInfoDiv = document.createElement('div');
-    
-        const productPrice = document.createElement('p');
-        productPrice.innerText = '$' + product.price;
-        const productName = document.createElement('p');
-        productName.innerText = product.name;
-    
-        productInfoDiv.appendChild(productPrice);
-        productInfoDiv.appendChild(productName);
-    
-        const productInfoFigure = document.createElement('figure');
-        const productImgCart = document.createElement('img');
-        productImgCart.setAttribute('src', './assets/icons/bt_add_to_cart.svg');
-    
-        productInfoFigure.appendChild(productImgCart);
-    
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
-    
-        productCard.appendChild(productImg);
-        productCard.appendChild(productInfo);
-    
-        cardsContainer.appendChild(productCard);
-    }
-}
-
-renderProducts(productList);
-*/
 
 
 
